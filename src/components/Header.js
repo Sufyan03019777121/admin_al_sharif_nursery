bebe72960e19f2fb99031f7e22fd9781b98de2b8
Sufyan03019777121 <156623479+Sufyan03019777121@users.noreply.github.com>
2025-04-25 07:07:49 +0500

    daraz5

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
     return (
       <header className="bg-primary text-white p-4">
         <div className="container">
-          <h1 className="m-0">Admin Panel</h1>
+          <h1 className="m-0">Al Sharif Nersery</h1>
         </div>
       </header>
     );
